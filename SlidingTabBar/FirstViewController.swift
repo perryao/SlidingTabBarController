eeb4b345aee86a835e6fe50fc15cae3845adce18
Mike Perry <perryao@uc.edu>
2015-06-19 09:43:59 -0400

    Initial Commit

---
+++ SlidingTabBar/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SlidingTabBar
+//
+//  Created by Mike on 6/19/15.
+//  Copyright © 2015 Mike Perry. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
